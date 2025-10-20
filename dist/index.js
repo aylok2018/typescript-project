aa717ecba892e8cd4c6d08cd47bf654b0efa3217
aylok2018 <aylok2018@gmail.com>
2025-10-20 09:10:22 +0300

    Initial TypeScript setup

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const userName = "Світ";
+const userAge = 25;
+const isActive = true;
+console.log(`Привіт, ${userName}! Твій вік: ${userAge}. Активний користувач: ${isActive}`);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const userName: string = "Світ";
+const userAge: number = 25;
+const isActive: boolean = true;
+
+console.log(`Привіт, ${userName}! Твій вік: ${userAge}. Активний користувач: ${isActive}`);
