f401964fe41a1d1a8588ee9491d9316b5ff60fcd
aylok2018 <114681246+aylok2018@users.noreply.github.com>
2025-11-09 18:44:20 +0200

    Delete src directory

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-const userName: string = "Світ";
-const userAge: number = 25;
-const isActive: boolean = true;
-
-console.log(`Привіт, ${userName}! Твій вік: ${userAge}. Активний користувач: ${isActive}`);
